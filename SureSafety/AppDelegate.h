13735a78fa8d11ed3a01a5bd4160525ee658becd
Xcode User <xcode@fasts-macbook-pro-6.local>
2017-10-10 09:18:57 -0400

    Initial Commit

---
+++ SureSafety/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SureSafety
+//
+//  Created by Xcode User on 2017-10-10.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SureSafety/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SureSafety
+//
+//  Created by Xcode User on 2017-10-10.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
