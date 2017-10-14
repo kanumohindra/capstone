721497780599b3bc486aa8a57ed392d74a9cdd73
Xcode User <xcode@fasts-macbook-pro-3.local>
2017-10-14 14:47:50 -0400

    added test tableview for dropdown menu

---
+++ SureSafety/SiteCell.swift
@@ -0,0 +1,44 @@
+//
+//  SiteCell.swift
+//  SureSafety
+//
+//  Created by Xcode User on 2017-10-14.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class SiteCell: UITableViewCell {
+    
+    let departmentName = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        
+        departmentName.textAlignment = NSTextAlignment.left
+        departmentName.font = UIFont.boldSystemFont(ofSize: 30)
+        departmentName.backgroundColor = UIColor.clear
+        departmentName.textColor = UIColor.black
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(departmentName)
+        
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SureSafety/TableTestViewController.swift
@@ -0,0 +1,75 @@
+//
+//  TableTestViewController.swift
+//  SureSafety
+//
+//  Created by Xcode User on 2017-10-14.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class TableTestViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var departments = ["Human Resources", "Financial", "Health and Safety", "Research and Development"]
+    
+    @IBOutlet var myTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return departments.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let tableCell : SiteCell = tableView.dequeueReusableCell(withIdentifier: "cell") as? SiteCell
+        ?? SiteCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        
+        let department = departments[indexPath.row]
+        tableCell.departmentName.text = department
+        
+        return tableCell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let indexPath = myTableView.indexPathForSelectedRow //optional, to get from any UIButton for example
+        
+        let currentCell = myTableView.cellForRow(at: indexPath!)! as! SiteCell
+        
+        print(currentCell.textLabel!.text ?? "test")
+        
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        myTableView.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
